81703a2d590bf1b730b28791b3a8ecd16f59ec49
Jena Grafton <JenaLGrafton@gmail.com>
2015-08-07 18:54:17 -0500

    Initial Commit

---
+++ Animal Ages Jena/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animal Ages Jena
+//
+//  Created by Jena Grafton on 8/7/15.
+//  Copyright © 2015 Bella Voce Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
